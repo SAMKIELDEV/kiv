3f4cd2e5fe12a6edd7f8970eba96c7e8880b376d
SAMKIEL <samuelezekiel488@gmail.com>
2026-05-06 07:51:15 +0100

    chore: update authsdk dependency source and remove redundant workbox configuration

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  workbox: {
-    skipWaiting: true,
-    clientsClaim: true,
-  },
   customWorkerSrc: "worker",
 });
 
