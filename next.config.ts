9bfd7bb642fe1b610c2c1a7de1860539da4cb9f2
SAMKIEL <samuelezekiel488@gmail.com>
2026-05-06 07:43:18 +0100

    chore: update workbox configuration to enable clientsClaim

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
+  workbox: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
   customWorkerSrc: "worker",
 });
 
